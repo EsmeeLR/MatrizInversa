893468d04ec98e01b8f1a67f2994dc8a43cec63a
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 18:11:08 -0600

    02 Método principal de la matriz inversa

--- MatrizInversa.java
+++ MatrizInversa.java
@@ -1,3 +1,21 @@
-public class MatriInversa{
-    
+import java.io.*;
+import java.util.Scanner;
+
+public class MatrizInversa {
+
+    // Método para calcular la inversa de una matriz cuadrada
+    public static double[][] inversa(double[][] matriz) {
+        int n = matriz.length;
+        double[][] identidad = new double[n][n];
+        double[][] copia = new double[n][n];
+
+        // Inicializar identidad y copia
+        for (int i = 0; i < n; i++) {
+            identidad[i][i] = 1;
+            for (int j = 0; j < n; j++) {
+                copia[i][j] = matriz[i][j];
+            }
+        }
+
+    }
 }
